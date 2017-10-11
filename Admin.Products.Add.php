4b0111c54eef585ac3ae18afca4e32b30ba9d3f8
Fredius Tout Court <frediustc@gmail.com>
2017-10-11 13:42:40 +0000

    config

---
+++ Admin.Products.Add.php
@@ -0,0 +1,26 @@
+<?php
+$page = 'Products';
+include 'incl/head.php'; ?>
+
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="header">
+                        <h4 class="title">Add New Product</h4>
+                        <p class="category">Field with (*) are required</p>
+                    </div>
+                    <div class="content table-responsive table-full-width">
+                        <form class="form" action="index.html" method="post">
+                            <div class="form-group">
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'incl/foot.php'; ?>
